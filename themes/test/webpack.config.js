0554a493f3ad026f0db7393059ae652a68e91a7f
白云苍狗 <admin@imalun.com>
2019-11-08 18:18:07 +0800

    0.15

---
+++ themes/test/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        'index': './src/index'
+    },
+    output: {
+        path: path.resolve(__dirname, 'source', 'js'),
+        filename: '[name].min.js',
+        publicPath: '/'
+    },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: require.resolve('babel-loader'),
+                        options: {
+                            compact: true,
+                            presets: ['env']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false,
+                ascii_only: true
+            },
+            sourceMap: true
+        })
+    ],
+}
